dd7c930f10450f415a1d57c3afc188d53d9e50a4
Tomas <tomas.ausvicas@topocentras.lt>
2019-01-03 10:31:19 +0200

    New File

---
+++ AirsoftCore.WebUI/Filters/ProductFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AirsoftCore.WebUI.Filters
+{
+    public class ProductFilter
+    {
+        public IList<int> Brands { get; set; }
+        public IList<int> Types { get; set; }
+        public int ShowOnly { get; set; }
+        public string Search { get; set; }
+    }
+}
